8bf1fb52a1663e437f53776fddcb8533ee15ed4c
Jeff Harrell <jeff@juxtadesign.com>
2012-10-10 22:39:04 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-DynamicButtonJS
-===============
 
-JavaScript for creating dynamic buttons for PayPal&#39;s checkout flows.
+
+
+
+# HTML payment buttons
+
+Integrating with our HTML payment buttons are as easy as including a snippet of code. We have two flavors of buttons for you to use:
+
+## Buy Now
+Buy Now buttons are for single item purchases.
+
+```html
+<script src="paypal.js"
+    data-paypal-checkout="buy-now"
+    data-paypal-button="true"
+    data-paypal-business="12345abc"
+    data-paypal-item-name="Buy me now!"
+    data-paypal-amount="1.00"
+></script>
+```
+
+Go ahead and try it out
+
+
+
+## Add To Cart
+Add To Cart buttons lets users add multiple items to their PayPal cart.
+
+```html
+<script src="paypal.js"
+    data-paypal-checkout="cart"
+    data-paypal-button="true"
+    data-paypal-business="12345abc"
+    data-paypal-item-name="Add me to cart"
+    data-paypal-amount="1.00"
+></script>
+```
+
+Prepare to be amazed with this one
+
+
+
+# API payment buttons
+
+```html
+<script src="paypal.js"
+    data-paypal-checkout="api"
+    data-paypal-button="true"
+></script>
+```
---
+++ paypal.js
@@ -0,0 +1,98 @@
+if (typeof PAYPAL === 'undefined' || !PAYPAL) {
+	var PAYPAL = {};
+}
+
+PAYPAL.apps = PAYPAL.apps || {};
+
+
+(function () {
+
+	'use strict';
+
+
+	function renderForm(el, data) {
+		var type = data.type,
+			isCart = (type === 'cart'),
+			form = document.createElement('form'),
+			input, btn, key;
+
+		form.method = 'post';
+		form.action = 'https://www.paypal.com/cgi-bin/webscr';
+
+		btn = document.createElement('input');
+		btn.type = 'image';
+
+		if (isCart) {
+			data.cmd = '_cart';
+			data.add = true;
+			btn.src = '//www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif';
+		} else {
+			data.cmd = '_xclick';
+			btn.src = '//www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif';
+		}
+
+		for (key in data) {
+			input = document.createElement('input');
+			input.type = 'hidden';
+			input.name = key;
+			input.value = data[key];
+
+			form.appendChild(input);
+		}
+
+		form.appendChild(btn);
+		el.parentNode.replaceChild(form, el);
+	}
+
+
+	function loadMiniCart() {
+		if (!PAYPAL.apps.MiniCart) {
+			var script = document.createElement('script');
+			script.src = 'http://www.minicartjs.com/build/minicart.js';
+			script.onload = function () {
+				if (PAYPAL.apps.MiniCart) {
+					PAYPAL.apps.MiniCart.render();
+				}
+			}
+
+			document.body.appendChild(script);
+		}
+	}
+
+
+	PAYPAL.apps.DynamicButton = (function () {
+		var app = {};
+
+		app.render = function () {
+			var nodes = document.getElementsByTagName('script'),
+				hasCart = false,
+				node, data, type, i, len;
+
+			for (i = 0, len = nodes.length; i < len; i++) {
+				node = nodes[i];
+				data = node.dataset;
+				type = data.type;
+
+				if (type) {
+					renderForm(node, data);
+				}
+
+				if (type === 'cart') {
+					hasCart = true;
+				}
+			}
+
+			if (hasCart) {
+				loadMiniCart();
+			}
+		};
+
+		return app;
+	}());
+
+
+}());
+
+
+PAYPAL.apps.DynamicButton.render();
+
