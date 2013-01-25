9a87a71bfb858e1aabd2bbf502753b171f124c0e
Jeff Harrell <jeharrell@paypal.com>
2013-01-24 17:17:20 -0800

    Cleaning up test cases

--- test/test.js
+++ test/test.js
@@ -4,14 +4,16 @@
 var fs = require('fs'),
 	should = require('should'),
 	jsdom = require('jsdom').jsdom,
-	document = jsdom(fs.readFileSync('./test/index.html').toString()),
+	jsdomOptions = { features: { QuerySelector: true }},
+	testFile = fs.readFileSync('./test/index.html').toString(),
+	document = jsdom(testFile, null, jsdomOptions),
 	window = document.createWindow();
 
 eval(fs.readFileSync('src/paypal-button.js').toString());
 
+
 // Test the object's integrity
 describe('PAYPAL.apps.ButtonFactory', function () {
-
 	'use strict';
 
 	it('Should have a PAYPAL object', function () {
@@ -28,9 +30,9 @@ describe('PAYPAL.apps.ButtonFactory', function () {
 
 });
 
+
 // Test the create method
 describe('PAYPAL.apps.ButtonFactory.create', function () {
-
 	'use strict';
 
 	it('Should be a function', function () {
@@ -44,80 +46,67 @@ describe('PAYPAL.apps.ButtonFactory.create', function () {
 	});
 });
 
+
 // Test the buttons counter object
 describe('PAYPAL.apps.ButtonFactory.buttons', function () {
-	
+
 	'use strict';
-	
+
 	var buttons = PAYPAL.apps.ButtonFactory.buttons;
-	
+
 	it('Should have four buy now buttons', function () {
-		buttons.buynow.should.equal(4);
+		buttons.buynow.should.equal(6);
 	});
-	
+
 	it('Should have three cart buttons', function () {
-		buttons.cart.should.equal(3);
+		buttons.cart.should.equal(2);
 	});
-	
+
 	it('Should have three hosted buttons', function () {
-		buttons.hosted.should.equal(3);
+		buttons.hosted.should.equal(2);
 	});
-	
+
 	it('Should have one QR code', function () {
 		buttons.qr.should.equal(1);
 	});
 });
 
+
+
 // Test multi-language support
 describe('Multi-language button images', function () {
-	
 	'use strict';
-	
-	it('Should have a spanish version of Buy Now button', function () {
-		var spanishButton = document.getElementById('buynowSpanish'),
-			spanishImage = spanishButton.getElementsByTagName('input')[0].src;
-		
-		spanishImage.should.include('es_ES');
-	});
-	
-	it('Should have a french version of Cart button', function () {
-		var frenchButton = document.getElementById('cartFrench'),
-			frenchImage = frenchButton.getElementsByTagName('input')[0].src;
-		
-		frenchImage.should.include('fr_FR');
-	});
-	
-	it('Should have a german version of Hosted button', function () {
-		var germanButton = document.getElementById('hostedGerman'),
-			germanImage = germanButton.getElementsByTagName('input')[0].src;
-		
-		germanImage.should.include('de_DE');
-	});
+
+	function testLanguage(locale, type) {
+		it('Should have a ' + locale + ' version of the ' + type + ' button', function () {
+			var image = document.querySelector('#' + type + '-' + locale + ' input[type="image"]'),
+				imagePath = image && image.src;
+
+			imagePath.should.include(locale);
+		});
+	}
+
+	testLanguage('es_ES', 'buynow');
+	testLanguage('fr_FR', 'buynow');
+	testLanguage('de_DE', 'buynow');
 });
+
 
 // Test multiple button image sizes
 describe('Multiple button image sizes', function () {
-	
 	'use strict';
-	
-	it('Should have a small version of Buy Now button', function () {
-		var buynowSmallButton = document.getElementById('buynowSmall'),
-			buynowSmallImg = buynowSmallButton.getElementsByTagName('input')[0].src;
-		
-		buynowSmallImg.should.include('SM');
-	});
-	
-	it('Should have a small version of Cart button', function () {
-		var cartSmallButton = document.getElementById('cartSmall'),
-			cartSmallImg = cartSmallButton.getElementsByTagName('input')[0].src;
-		
-		cartSmallImg.should.include('SM');
-	});
-	
-	it('Should have a small version of Hosted button', function () {
-		var hostedSmallButton = document.getElementById('hostedSmall'),
-			hostedSmallImg = hostedSmallButton.getElementsByTagName('input')[0].src;
-		
-		hostedSmallImg.should.include('SM');
-	});
+
+	function testSize(size, type) {
+		it('Should have a ' + size + ' version of ' + type + ' button', function () {
+			var image = document.querySelector('#' + type + '-' + size + ' input[type="image"]'),
+				imagePath = image && image.src;
+
+			imagePath.should.include('SM');
+		});
+	}
+
+	testSize('sm', 'buynow');
+	testSize('sm', 'cart');
+	testSize('sm', 'hosted');
 });
+
