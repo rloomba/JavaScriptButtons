97bb32a979d8ae2d9f804cac3b2b89a7c1000390
Jeff Harrell <jeff@juxtadesign.com>
2013-03-15 21:29:17 -0700

    Updating tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,19 +13,35 @@ module.exports = function (grunt) {
 				}
 			}
 		},
+		concat: {
+			dist: {
+				src: [ "<banner:meta.banner>", "src/paypal-button.js" ],
+				dest: "dist/paypal-button.js",
+				options: {
+					banner: "/*!\n * <%= pkg.name %>\n * <%= pkg.description %>\n * @version <%= pkg.version %> - <%= grunt.template.today(\'yyyy-mm-dd\') %>\n * @author <%= pkg.author.name %> <<%= pkg.author.url %>>\n */\n"
+				}
+			},
+			bundled: {
+				src: [ "<banner:meta.banner>", "lib/MiniCart/minicart.js", "src/paypal-button.js" ],
+				dest: "dist/paypal-button-minicart.js",
+				options: {
+					banner: "/*!\n * <%= pkg.name %>\n * <%= pkg.description %>\n * @version <%= pkg.version %> - <%= grunt.template.today(\'yyyy-mm-dd\') %>\n * @author <%= pkg.author.name %> <<%= pkg.author.url %>>\n */\n"
+				}
+			}
+		},
 		uglify: {
 			dist: {
 				src: [ "<banner:meta.banner>", "src/paypal-button.js" ],
 				dest: "dist/paypal-button.min.js",
 				options: {
-					banner: "/*!\n * <%= pkg.name %>\n * <%= pkg.description %>\n * @version <%= pkg.version %> - <%= grunt.template.today(\'yyyy-mm-dd, h:MM:ss TT\') %>\n * @author <%= pkg.author.name %> <<%= pkg.author.url %>>\n */"
+					banner: "/*!\n * <%= pkg.name %>\n * <%= pkg.description %>\n * @version <%= pkg.version %> - <%= grunt.template.today(\'yyyy-mm-dd\') %>\n * @author <%= pkg.author.name %> <<%= pkg.author.url %>>\n */\n"
 				}
 			},
 			bundled: {
 				src: [ "<banner:meta.banner>", "lib/MiniCart/minicart.js", "src/paypal-button.js" ],
 				dest: "dist/paypal-button-minicart.min.js",
 				options: {
-					banner: "/*!\n * <%= pkg.name %>\n * <%= pkg.description %>\n * @version <%= pkg.version %> - <%= grunt.template.today(\'yyyy-mm-dd, h:MM:ss TT\') %>\n * @author <%= pkg.author.name %> <<%= pkg.author.url %>>\n */"
+					banner: "/*!\n * <%= pkg.name %>\n * <%= pkg.description %>\n * @version <%= pkg.version %> - <%= grunt.template.today(\'yyyy-mm-dd\') %>\n * @author <%= pkg.author.name %> <<%= pkg.author.url %>>\n */\n"
 				}
 			}
 		}
@@ -34,8 +50,9 @@ module.exports = function (grunt) {
 	// Load grunt tasks from npm packages
 	grunt.loadNpmTasks("grunt-contrib-jshint");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-concat");
 
 	// Default task.
-	grunt.registerTask("default", ["jshint", "uglify"]);
+	grunt.registerTask("default", ["jshint", "concat", "uglify"]);
 
 };
