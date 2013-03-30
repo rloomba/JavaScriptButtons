1116404b4958f52b749c09d089089139ba3bea92
mstuart <stuartmark@gmail.com>
2013-03-30 13:05:12 -0700

    Added grunt-update-submodules task.  Will update all submodules (MiniCart) before running concat and uglify tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,8 +54,9 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks("grunt-contrib-jshint");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks('grunt-update-submodules');
 
 	// Default task.
-	grunt.registerTask("default", ["jshint", "concat", "uglify"]);
+	grunt.registerTask("default", ["jshint", "update_submodules", "concat", "uglify"]);
 
 };
